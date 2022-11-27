00f3910d23d44bb7e1a7cedc7d7682522793f3c2
MathxH Chen <brainfvck@foxmail.com>
2022-11-27 17:17:55 +0800

    Impl Encrypt and Decrypt for message

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 
 
 use num_bigint::{BigInt, BigUint, RandBigInt, ToBigInt};
-use num_integer::Integer;
 use num_primes::Generator;
 use num_traits::identities::{One, Zero};
 use std::{clone::Clone};
@@ -36,13 +35,16 @@ pub fn make_key_pair(bitlen: usize) -> Option<KeyPair> {
     // but If using p,q of equivalent length, 
     // a simpler variant of the above key generation steps would be to set
     // g = n + 1, lambda = phi(n), mu = phi(n)^-1 mod n,  phi(n) = (p - 1)*(q - 1)
+    // The simpler variant is recommended for implementational purposes, 
+    // because in the general form the calculation time of mu
+    // can be very high with sufficiently large primes p,q.
     // https://crypto.stackexchange.com/questions/8276/what-does-mathbbz-n2-mean
     let g = n.clone() + BigInt::one();
 
     // lambda = phi(n)
     let lambda = phi(&p, &q);
     
-    // mu = phi(n)^-1 = lambda^-1
+    // mu = phi(n)^-1 mod n = lambda^-1 mod n = inverse lambda mod n 
     let mu = mod_inverse(&lambda, &n).unwrap();
 
     let pk = PubKey {
@@ -56,6 +58,54 @@ pub fn make_key_pair(bitlen: usize) -> Option<KeyPair> {
     ));
 }
 
+impl PubKey {
+    /// encrypt message to ciphertext
+    pub fn encrypt_message(&self, msg: &str) -> Option<BigInt> {
+        let msg_int = BigUint::from_bytes_be(msg.as_bytes()).to_bigint().unwrap();
+        self.encrypt(&msg_int)
+    }
+
+    /// ecrypt integer message to ciphertext
+    pub fn encrypt(&self, m: &BigInt) -> Option<BigInt> {
+        // Let m be a message to be encrypted where 0 <= m < n
+        if m >= &BigInt::zero() && m < &self.n {
+            // select random r where 0 < r < n
+            let mut rng = rand::thread_rng();
+            let r =  rng.gen_bigint_range(&BigInt::one(), 
+                &self.n);
+            
+            // compute ciphertext c = g^m * r^n (mod n^2)
+            let g_m = self.g.modpow(m, &self.nn);
+            let r_n = r.modpow(&self.n, &self.nn);
+            let c = (g_m * r_n) % self.nn.clone();
+            Some(c)
+        } else {
+            None
+        }
+    }
+}
+
+impl PrivKey {
+    pub fn decrypt_message(&self, ciphertext: &BigInt) -> Option<String> {
+        let plain_text_int = self.decrypt(ciphertext).unwrap();
+        Some(String::from_utf8(plain_text_int.to_biguint().unwrap().to_bytes_be()).unwrap())
+    }
+
+    pub fn decrypt(&self, ciphertext: &BigInt) -> Option<BigInt> {
+        // Let c be the ciphertext to decrypt, where c ∈ Z_(n^2)*    0 < c <= n^2 - 1
+        if ciphertext == &BigInt::zero() {
+            None
+        } else {
+           // Compute the plaintext message as m = L(c^lambda mod n^2) * mu mod n
+           let c_lambda = ciphertext.modpow(&self.lambda, &self.pk.nn);
+           let l_ = l(&c_lambda, &self.pk.n);
+           let m = (l_ * self.mu.clone()) % self.pk.n.clone();
+           Some(m)
+        }
+    }
+}
+
+// L(x) = (x - 1) / n
 fn l(x: &BigInt, n: &BigInt) -> BigInt {
     let x1 = x - BigInt::one();
     x1 / n
@@ -65,10 +115,6 @@ fn phi(p: &BigInt, q: &BigInt) -> BigInt {
     let p1 = p.sub(BigInt::one());
     let q1 = q.sub(BigInt::one());
     p1 * q1
-}
-
-fn g_lambda(g:&BigInt, lambda: &BigInt, nn: &BigInt) -> BigInt {
-    g.modpow(lambda, nn)
 }
 
 fn extend_gcd(a: BigInt, b: BigInt) -> (BigInt, BigInt, BigInt) {
